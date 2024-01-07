f9d3133d80c73e3be203cf2a5f3b341423cb60a3
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2024-01-07 20:54:42 +0200

    add

--- routes/product.js
+++ routes/product.js
@@ -4,7 +4,112 @@ import productImagesUpload from "../middleware/productMulter";
 
 const productsRouter = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     products:
+ *       type: object
+ *       required:
+ *         - title
+ *         - url
+ *         - thumbnailUrl
+ *         - price
+ *         - categoryId
+ *         - userId
+ *         - Longitude
+ *         - Latitude
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: The title of the product
+ *         url:
+ *           type: string
+ *           format: binary
+ *           description: The product image url
+ *         thumbnailUrl:
+ *           type: string
+ *           format: binary
+ *           description: The product image thumbnailUrl
+ *         price:
+ *           type: string
+ *           description: The price of the product
+ *         categoryId:
+ *           type: string
+ *           description: The category id of the product
+ *         userId:
+ *           type: string
+ *           description: The user id of the product
+ *         Longitude:
+ *           type: string
+ *           description: The Longitude of the user location
+ *         Latitude:
+ *           type: string
+ *           description: The Latitude of the user location
+ *       example:
+ *         title: "Red jacket"
+ *         url: "jacket.jpg"
+ *         thumbnailUrl: "jacket.jpg"
+ *         price: 100
+ *         categoryId: 5
+ *         userId: 1
+ *         Longitude: "30.0474° E"
+ *         Latitude: "-1.9706° S"
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: The products managing API
+ */
+
+/**
+ * @swagger
+ * /AguraMarket/products/addNewProduct:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     requestBody:
+ *          required: true
+ *          content:
+ *            multipart/form-data:
+ *               schema:
+ *                   $ref: '#/components/schemas/products'
+ *
+ *     responses:
+ *       200:
+ *          description: The product created successfully
+ *          content:
+ *             multipart/form-data:
+ *               schema:
+ *                   $ref: '#/components/schemas/products'
+ *       500:
+ *          description: Internal Server Error
+ */
+
 productsRouter.post("/addNewProduct", productImagesUpload, addNewProduct);
+
+/**
+ * @swagger
+ *  /AguraMarket/products/getAllProducts:
+ *   get:
+ *     summary: Returns the list of all products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *          description: The products found successfully
+ *          content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   $ref: '#/components/schemas/products'
+ *       404:
+ *          description: Not found
+ *       500:
+ *          description: Internal Server Error
+ */
 
 productsRouter.get("/getAllProducts", getAllProducts);
 
